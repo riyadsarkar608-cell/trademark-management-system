64555e8310a98243a59aaf9efc5f79bff7988ff9
riyadsarkar608-cell <riyadsarkar608@gmail.com>
2026-06-03 12:20:39 +0600

    Landing Page Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <main style={{ padding: "40px" }}>
-      <h1>Trademark Management System</h1>
-      <p>Supabase Connected Successfully</p>
+    <main className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">
+        Trademark Management System
+      </h1>
+
+      <p className="text-gray-300 mb-8">
+        User & Admin Dashboard
+      </p>
+
+      <div className="flex gap-4">
+        <button className="bg-blue-600 px-6 py-3 rounded-lg">
+          User Login
+        </button>
+
+        <button className="bg-green-600 px-6 py-3 rounded-lg">
+          Admin Login
+        </button>
+      </div>
     </main>
   );
 }
