16d2eb3f116c73b953e42c3c217d10c60c0e1915
riyadsarkar608-cell <riyadsarkar608@gmail.com>
2026-06-03 12:56:42 +0600

    Login Page Added

---
+++ app/login/page.tsx
@@ -0,0 +1,27 @@
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <div className="bg-slate-800 p-8 rounded-lg w-96">
+        <h1 className="text-3xl font-bold mb-6">
+          User Login
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-3 mb-4 rounded text-black"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 mb-4 rounded text-black"
+        />
+
+        <button className="w-full bg-blue-600 p-3 rounded">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
