3576c7ac5ff7c42a5ec75feb3069b6e7bafdac6a
riyadsarkar608-cell <riyadsarkar608@gmail.com>
2026-06-03 13:05:19 +0600

    Supabase Register Added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,30 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Registration Successful! Check your email.");
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
       <div className="bg-slate-800 p-8 rounded-lg w-96">
         <h1 className="text-3xl font-bold mb-6">
-          User Registration
+          Register
         </h1>
-
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="w-full p-3 mb-4 rounded text-black"
-        />
 
         <input
           type="email"
           placeholder="Email"
           className="w-full p-3 mb-4 rounded text-black"
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
           type="password"
           placeholder="Password"
           className="w-full p-3 mb-4 rounded text-black"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="w-full bg-green-600 p-3 rounded">
+        <button
+          onClick={signUp}
+          className="w-full bg-green-600 p-3 rounded"
+        >
           Register
         </button>
       </div>
